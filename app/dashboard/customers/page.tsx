ba54db554faef7c5881e0e42d53833b5b16f36ad
GW00265321 <wanliang123>
2025-02-24 17:01:56 +0800

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>custom page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter,Lustria } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lustria({
+    weight: '400',
+    subsets: ['latin'],
+});
